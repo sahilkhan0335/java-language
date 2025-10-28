f1deae5be208e7b7fd629f80c833e09efed78fc2
sahilkhan0335 <sahil233582@gmail.com>
2025-10-28 10:02:50 +0530

    Completed Java Basic With Type Conversion

---
+++ Variables & Data Types/TypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+// Float Into Int  Type Casting
+ public class TypeCasting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float number1 = 25.999f;
+        int number2 = (int) number1;
+        System.out.println(number2);
+    }
+ }
---
+++ Variables & Data Types/TypeCastingChar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+// Char  Into Int  Type Casting
+ public class TypeCastingChar {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+         char ch = 'a';
+         char ch2 = 'b';
+        int number1 = ch;
+        int number2 = ch2;
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+ }
