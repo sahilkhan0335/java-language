8864bdfdd6a8b3116f1870320b2467ab20005134
sahilkhan0335 <sahil233582@gmail.com>
2025-10-28 17:24:29 +0530

    JavaBasic Operators( Assignment  Operators)

---
+++ Operators/Assignment Operators/AddAssi.java
@@ -0,0 +1,7 @@
+public class AddAssi {
+    public static void main(String arge[]) {
+       int A = 10;
+       A += 10;
+       System.out.println((A));
+   }
+}
---
+++ Operators/Assignment Operators/SubAssi.java
@@ -0,0 +1,7 @@
+public class SubAssi {
+    public static void main(String arge[]) {
+        int A = 10;
+        A -= A;
+        System.out.println( (A) );
+    }
+}
