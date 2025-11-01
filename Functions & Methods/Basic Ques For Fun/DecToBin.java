428dfbc71e33272d32e1086adc0befb8b1abc6a4
sahilkhan0335 <sahil233582@gmail.com>
2025-11-01 15:08:12 +0530

    Java Function and Methods ( Functions/Parameters/Callbyvalue/Overloading/Example

---
+++ Functions & Methods/Basic Ques For Fun/DecToBin.java
@@ -0,0 +1,40 @@
+public class DecToBin {
+
+    // Binary To Decimal Conversion 
+    public static void bintodec (int binNum ) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+
+            pow++;
+            binNum = binNum/10;
+        }
+
+        System.out.println("Decimal of " + myNum + " = " + decNum);
+    }
+     
+     // Decimal To Binary Conversion 
+     public static void decTobin ( int n ) {
+        int myNum = n;
+        int pow = 0;
+        int binNum = 0;
+
+        while ( n > 0) {
+            int rem = n % 2;
+            binNum = binNum + (rem * (int)Math.pow(10, pow));
+            pow++;
+            n = n/2;
+        }
+        System.out.println("Binary Form Of " + myNum + " = " + binNum);
+
+     }
+       
+    public static void main (String args[]) {
+       decTobin(7);
+       decTobin(12);
+    }
+}
