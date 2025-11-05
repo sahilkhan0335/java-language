b4e3be5fb0ad9afeca478de61e3a5b0a9a7429b3
sahilkhan0335 <sahil233582@gmail.com>
2025-11-05 08:25:54 +0530

    Java Array (Basic/Linear/Binary Search or Some Examples

---
+++ Array/Binary Search/SubArr.java
@@ -0,0 +1,44 @@
+public class SubArr {
+
+    public static void subArrays(int numbers[]) {
+        int ts = 0; // total subarrays
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+
+        // Generate all subarrays
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int sum = 0;
+
+                // Print subarray and calculate sum
+                for (int k = start; k <= end; k++) {
+                    System.out.print(numbers[k] + " ");
+                    sum += numbers[k];
+                }
+
+                // Update max and min sums
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+                if (sum < minSum) {
+                    minSum = sum;
+                }
+
+                ts++;
+                System.out.println("=> Sum = " + sum);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Total SubArrays = " + ts);
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+        System.out.println("Minimum Subarray Sum = " + minSum);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10, 12};
+        subArrays(numbers);
+    }
+}
