ed488fc23002a18124ca9fd857655bca52940c24
sahilkhan0335 <sahil233582@gmail.com>
2025-11-07 19:32:13 +0530

    Java Array (Basic/Linear/Binary Search/ Added Subarray Exmples

---
+++ Array/Exmples/Kadanes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Kadanes {
+
+    public static void KadanesNeg (int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<numbers.length; i++) {
+            cs = cs +numbers[i];
+            if( cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs ,ms);
+        }
+        System.out.println("Our Max Subarryas Sum Is : " + ms);
+    } 
+    public static void main(String args[]) {
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        KadanesNeg(numbers);
+    }
+}
